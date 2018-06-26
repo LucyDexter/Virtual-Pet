47df058d09a55f2d87c39fd64995bfc088287e73
Lucy Bradford <looc07@hotmail.co.uk>
2018-06-26 15:14:05 +0100

    keeping fit completed

--- src/pet.js
+++ src/pet.js
@@ -1,3 +1,4 @@
+const MAXIMUM_FITNESS = 10;
 function Pet(name) {
     this.name = name;
     this.age = 0;
@@ -7,6 +8,18 @@ function Pet(name) {
         this.fitness -= 3;
     }
     this.hunger = 0;
-    this.fitness = 10;
+    this.fitness = MAXIMUM_FITNESS;
+}
+
+Pet.prototype.walk = function() {
+    this.fitness += 4;
+}
+
+Pet.prototype.walk = function() {
+    if ((this.fitness + 4) <= MAXIMUM_FITNESS) {
+        this.fitness += 4;
+    } else {
+        this.fitness = MAXIMUM_FITNESS;
+    }
 }
 module.exports = Pet;

849c694c297d2ddeef483701c0968d914c1a9c0b
Lucy Bradford <looc07@hotmail.co.uk>
2018-06-26 12:31:53 +0100

    set the hunger property

--- _tests_/pet.test.js
+++ _tests_/pet.test.js
@@ -29,3 +29,20 @@ describe('growUp', () => {
         expect(pet.age).toEqual(1);
     });
 });
+
+
+describe('constructor', () => {
+
+    it('has an initial hunger of 0', () => {
+        const pet = new Pet('Fido');
+        expect(pet.hunger).toEqual(0);
+    })
+})
+
+describe('growUp', () => {
+    it('increments the hunger by 5', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.hunger).toEqual(5);
+    })
+})
--- src/pet.js
+++ src/pet.js
@@ -3,6 +3,8 @@ function Pet(name) {
     this.age = 0;
     this.growUp = function () {
         this.age += 1;
+        this.hunger += 5;
     }
+    this.hunger = 0;
 }
 module.exports = Pet;
